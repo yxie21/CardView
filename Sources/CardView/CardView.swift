373ffb9405d8edc2ec0b38de57cc9b67edb8a07a
nzufelt <nicholas.zufelt@gmail.com>
2020-09-24 14:55:44 -0400

    allow for different choices of color, defaulting to Color.gray still. Non-breaking change.

--- Sources/CardView/CardView.swift
+++ Sources/CardView/CardView.swift
@@ -3,14 +3,16 @@ import SwiftUI
 @available(iOS 13.0, macOS 10.15, *)
 public struct CardView<Content: View>: View {
     var content: () -> Content
+    var color: Color
     
-    public init(@ViewBuilder content: @escaping () -> Content) {
+    public init(color: Color = Color.gray, @ViewBuilder content: @escaping () -> Content) {
         self.content = content
+        self.color = color
     }
     
     public var body: some View {
         ZStack {
-            Color.gray
+            self.color
                 .clipShape(RoundedRectangle(cornerRadius: 25.0))
                 .frame(maxWidth: 200, maxHeight: 150)
             VStack {
